9521cef7109a07edc19772f057bd5ffe61ff580c
lsy3709 <lsy3709@naver.com>
2022-11-16 12:41:04 +0900

    221116 4교시 예제 저장3 오타 수정 All 빠져서

--- 221116/colors.js
+++ 221116/colors.js
@@ -10,7 +10,7 @@
 
    var Links = {
     setColor: function(color) {
-     var alist = document.querySelector('a');
+     var alist = document.querySelectorAll('a');
      var i = 0;
      while(i < alist.length) {
      alist[i].style.color = color;
@@ -53,7 +53,7 @@
                 Body.setColor('white');
                 self.value = 'day'
                 //setColor('powderblue');
-                Links.setColor('powderblue')
+                Links.setColor('red')
                
             } else {
               //  target.style.backgroundColor = 'white';
